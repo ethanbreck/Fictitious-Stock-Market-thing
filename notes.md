088782bf7873bea9bb5be3d77815713b31a83ba4
Ethan Breckenridge <ekbreckenridge@me.com>
2021-01-27 17:54:28 -0800

    update stuff

--- notes.md
+++ notes.md
@@ -3,3 +3,5 @@
 Are Numpy and Pandas neeeded?
 
 Does STOCH mean The Stochastic Oscillator? https://www.tradingview.com/support/solutions/43000502332-stochastic-stoch/
+
+Why is ```prices["Date"] = mdates.date2num(prices.index)  # creates a date column stored in number format (for OHCL bars)``` there? Can the normal date not be used for datetime stuff? Can the datetime string not be converted in real time for each datapoint? Why is this datapoint used for mathplotlib? 
--- test.py
+++ test.py
@@ -20,43 +20,21 @@ yf.pdr_override()  # activate yahoo finance workaround
 
 stock = input("Enter the stock symbol : ")  # Asks for stock ticker
 
-
-# Functions:
-
-
-def smasUsed():
-    # Choose smas
-    return [10, 30, 50]
+prices = None
+app_dir = os.path.abspath(os.path.dirname(__file__))
 
 
-def dataframeStart():
-    # Sets start point of dataframe
-    smas = smasUsed()
-    start = datetime.datetime(2020, 1, 1) - datetime.timedelta(days=max(smas))
-    return start
-
-
-def getData():
+def getData(smas):
     now = datetime.datetime.now()
-    start = dataframeStart()
+    start = datetime.datetime(2020, 1, 1) - datetime.timedelta(days=max(smas))
     data = pdr.get_data_yahoo(stock, start, now)
     return data
 
-
 # Code
 
-prices = getData()
 
-app_dir = os.path.abspath(os.path.dirname(__file__))
-startingData = os.path.join(app_dir, "data/data.csv")
-
-with open(startingData, mode="a") as file:
-    data = prices.to_csv(file)
-    # can do JSON as well by switching filetype to .JSON after the file name, and switch prices.to_csv(file) to prices.to_json(file)
-
-
-def smaCalc():
-    theSma = smasUsed()
+def smaCalc(smaNums):
+    theSma = smaNums
     for x in theSma:
         prices['SMA_' + str(x)] = prices.iloc[:, 4].rolling(window=x).mean()
     smaData = os.path.join(app_dir, "data/data_w_sma.csv")
@@ -67,17 +45,42 @@ def smaCalc():
     return None
 
 
-def bBands():
-    # calculate Bollinger Bands
-    BBperiod = 15
-    stdev = 2  # choose moving averave
+def smaStdev(bbperiod):
+    BBperiod = bbperiod
     prices['SMA' + str(BBperiod)] = prices.iloc[:, 4].rolling(window=BBperiod).mean()  # calculates sma and creates a column in the dataframe
     prices['STDEV'] = prices.iloc[:, 4].rolling(window=BBperiod).std()  # calculates standard deviation and creates col
+    return None
+
+
+def lowerBand(bbperiod, stdev):
+    BBperiod, stdev = bbperiod, stdev
     prices['LowerBand'] = prices['SMA' + str(BBperiod)] - (stdev * prices['STDEV'])  # calculates lower bollinger band
+    return None
+
+
+def upperBand(bbperiod, stdev):
+    BBperiod, stdev = bbperiod, stdev
     prices['UpperBand'] = prices['SMA' + str(BBperiod)] + (stdev * prices['STDEV'])  # calculates upper band
-    prices["Date"] = mdates.date2num(prices.index)    # creates a date column stored in number format (for OHCL bars)
-    bbData = os.path.join(app_dir, "data/data_w_bbands.csv")
-    with open(bbData, mode="w") as file:
+    return None
+
+
+def main():
+    global prices
+    smasused = 10, 30, 50  # This is for simple moving average's
+    BBperiod = 15  # chooses Bollinger Bands
+    stdev = 2  # choose moving average
+    prices = getData(smasused)
+    startingData = os.path.join(app_dir, "data/data.csv")
+    with open(startingData, mode="a") as file:
+        prices.to_csv(file)
+    # can do JSON as well by switching filetype to .JSON after the file name, and switch prices.to_csv(file) to prices.to_json(file)
+    smaCalc(smasused)
+    smaStdev(BBperiod)
+    lowerBand(BBperiod, stdev)
+    upperBand(BBperiod, stdev)
+    prices["Date"] = mdates.date2num(prices.index)  # creates a date column stored in number format (for OHCL bars)
+    bbanddata = os.path.join(app_dir, "data/data_w_bbands.csv")
+    with open(bbanddata, mode="w") as file:
         prices.to_csv(file)
         # can do JSON as well by switching filetype to .JSON after the file name, and switch prices.to_csv(file) to prices.to_json(file)
 
@@ -85,7 +88,4 @@ def bBands():
 
 
 if __name__ == '__main__':
-    smaCalc()
-    print("SMA has been added")
-    bBands()
-    print("Bollinger Bands have been calculated")
+    main()
